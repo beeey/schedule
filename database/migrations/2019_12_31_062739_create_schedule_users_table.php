b77f283a5fdfb2fd03ea55f32dde5bdcde78bf70
Beeey <bs_magicer_064@yahoo.co.jp>
2019-12-31 19:41:14 +0900

    テーブル作成

--- database/migrations/2019_12_31_062739_create_schedule_users_table.php
+++ database/migrations/2019_12_31_062739_create_schedule_users_table.php
@@ -14,6 +14,8 @@ class CreateScheduleUsersTable extends Migration
     public function up()
     {
         Schema::create('schedule_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('schedule_id');
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users');
