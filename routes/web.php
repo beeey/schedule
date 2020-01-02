1912f62f88730958cdb9bb5ef60e0d5352e2d466
Beeey <bs_magicer_064@yahoo.co.jp>
2020-01-03 04:59:48 +0900

    iroiro

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::get('/', function () {
         return view('welcome');
-    });
+    })->name('login');
 });
